cca31a89e1979cf8640e41dd757152ae63ff2063
Philippe Lambet <philippe.lambert.fr@gmail.com>
2020-01-13 13:38:30 +0100

    add a div to seperate video et component articlecard

--- src/components/ArtcileList.jsx
+++ src/components/ArtcileList.jsx
@@ -1,36 +1,43 @@
 import React from "react";
 import ArticleCard from "./ArticleCard";
+import "./style/ArticleList.scss";
 import VideoBg from "./VideoBg";
 
 function ArticleList() {
   return (
-    <div>
+    <div className="article-list">
       <VideoBg />
-      {article.map(item => (
-        <ArticleCard image={item.image} title={item.title} lorum={item.lorum} />
-      ))}
+      <div className="article-square">
+        {article.map(item => (
+          <ArticleCard
+            image={item.image}
+            title={item.title}
+            lorum={item.lorum}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
 const article = [
   {
-    image: "./Images/tokyo-article-place.jpg",
+    image: "./Images/visit.jpg",
     title: "VISIT",
     lorum: "Discover the best secret place only knew by Tokyoiste people "
   },
   {
-    image: "./Images/tokyo-article-place.jpg",
+    image: "./Images/activities.jpg",
     title: "ACTIVITIES",
     lorum: "Do what is only existing in Tokyo!"
   },
   {
-    image: "Images/tokyo-article-bento.jpg",
+    image: "Images/eatanddrink.jpg",
     title: "EAT&DRINK",
     lorum: "The most liked place by local people"
   },
   {
-    image: "Images/tokyo-article-game.jpg",
+    image: "Images/wtf.jpg",
     title: "WTF",
     lorum: "Japan reserve unique and crazy experience"
   }
